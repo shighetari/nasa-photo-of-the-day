aaa8687d03439a23adb42b96cd29c2f668700961
Francisco Barrios <shighetari@gmail.com>
2020-05-14 18:07:56 -0400

    project day 2 done

--- src/WorkFile.js
+++ src/WorkFile.js
@@ -1,11 +1,18 @@
 import React, { useState, useEffect } from 'react'
 import Header from "./Header.js";
+import Desc from "./Desc.js";
+import makeStyles from "./App.js"
+
+
 export default function Post(props) {
 // console.log(props)
     return (
         <div className="post">
             <Header title={props.title} date={props.date} />
             <img src={props.url} className="nasaImg" alt={props.title} />
+           
+           <Desc desc = {props.desc} ></Desc>
+            
         </div>
     )
 }
